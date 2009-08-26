e2f84236a708f1a2b65bc304fbb0034ee6b2124e
Erica Sadun <ericasadun@Banana.local>
2009-08-26 14:07:50 -0600

    First Commit

---
+++ main.m
@@ -0,0 +1,57 @@
+/*
+ Erica Sadun, http://ericasadun.com
+ iPhone Developer's Cookbook, 3.0 Edition
+ BSD License, Use at your own risk
+ */
+
+#import <UIKit/UIKit.h>
+#import "NSFileManager-Utilities.h"
+
+#define COOKBOOK_PURPLE_COLOR	[UIColor colorWithRed:0.20392f green:0.19607f blue:0.61176f alpha:1.0f]
+#define BARBUTTON(TITLE, SELECTOR) 	[[[UIBarButtonItem alloc] initWithTitle:TITLE style:UIBarButtonItemStylePlain target:self action:SELECTOR] autorelease]
+
+@interface TestBedViewController : UIViewController
+@end
+
+@implementation TestBedViewController
+
+- (void) action: (id) sender
+{
+	NSString *file;
+	NSMutableArray *results = [NSMutableArray array];
+#if !TARGET_IPHONE_SIMULATOR
+	NSDirectoryEnumerator *dirEnum = [[NSFileManager defaultManager] enumeratorAtPath:@"/var/mobile/Media/DCIM"];
+#else
+	NSDirectoryEnumerator *dirEnum = [[NSFileManager defaultManager] enumeratorAtPath:[NSHomeDirectory() stringByAppendingPathComponent:@"../../Media/DCIM"]];
+#endif
+	while (file = [dirEnum nextObject]) [results addObject:file];
+	CFShow(results);
+	
+}
+
+- (void) viewDidLoad
+{
+	self.navigationController.navigationBar.tintColor = COOKBOOK_PURPLE_COLOR;
+	self.navigationItem.rightBarButtonItem = BARBUTTON(@"Go", @selector(action:));
+}
+@end
+
+@interface TestBedAppDelegate : NSObject <UIApplicationDelegate>
+@end
+
+@implementation TestBedAppDelegate
+- (void)applicationDidFinishLaunching:(UIApplication *)application {	
+	UIWindow *window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[[TestBedViewController alloc] init]];
+	[window addSubview:nav.view];
+	[window makeKeyAndVisible];
+}
+@end
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, @"TestBedAppDelegate");
+	[pool release];
+	return retVal;
+}
